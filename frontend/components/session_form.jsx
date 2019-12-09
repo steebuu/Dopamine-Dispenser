c5c4d4e149c95019f653d27189dd97eefffbea9a
App Academy Student <email@email.com>
2019-12-09 17:08:32 -0500

    Setup for deploying to Heroku

--- frontend/components/session_form.jsx
+++ frontend/components/session_form.jsx
@@ -14,7 +14,6 @@ class SessionForm extends React.Component {
   }
 
   handleSubmit(e) {
-    // debugger;
     e.preventDefault();
     const user = Object.assign({}, this.state);
     this.props.processForm(user);
