43c45d5fd45d6c1d775c9361c3f70efb4a149af9
App Academy Student <email@email.com>
2019-12-19 21:55:37 -0500

    Trying to fix signup issues

--- frontend/components/userauth/session_form.jsx
+++ frontend/components/userauth/session_form.jsx
@@ -20,6 +20,7 @@ class SessionForm extends React.Component {
     e.preventDefault();
     const user = Object.assign({}, this.state);
     this.props.processForm(user);
+    this.props.history.push("/");
   }
 
   handleChange(field) {
--- frontend/components/userauth/signup_form_container.js
+++ frontend/components/userauth/signup_form_container.js
@@ -1,4 +1,5 @@
 import { connect } from 'react-redux';
+import {withRouter} from 'react-router-dom';
 import SessionForm from './session_form';
 import { login, signup, clearErrors, logout } from '../../actions/session_actions';
 
