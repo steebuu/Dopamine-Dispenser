814a2ddfe340d2aad2590a78f30c747db1caf11e
Steve <T.AllGameMail@protonmail.com>
2020-01-23 15:59:26 -0500

    creates follows table

---
+++ app/models/follow.rb
@@ -0,0 +1,25 @@
+# == Schema Information
+#
+# Table name: follows
+#
+#  id          :bigint           not null, primary key
+#  follower_id :integer          not null
+#  followed_id :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
+class Follow < ApplicationRecord
+    validates :follower_id, presence: true
+    validates :followed_id, presence: true
+
+    belongs_to :follower,
+    class_name: :User,
+    foreign_key: :id,
+    primary_key: :follower_id
+
+    belongs_to :followed,
+    class_Name: :User,
+    foreign_key: :id,
+    primary_key: :followed_id
+end
