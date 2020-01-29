20b43e79fb5964dbd39a93ff799365e4a32967ae
Steve <T.AllGameMail@protonmail.com>
2020-01-29 17:51:11 -0500

    adds user assocations for followed/followers

--- app/models/user.rb
+++ app/models/user.rb
@@ -51,15 +51,23 @@ class User < ApplicationRecord
     foreign_key: :user_id,
     primary_key: :id
 
+  has_many :follower_relationships,
+    class_name: :Follow,
+    foreign_key: :followed_id,
+    primary_key: :id
+
   has_many :followers,
+    through: :follower_relationships,
+    source: :follower
+
+  has_many :followed_relationships,
     class_name: :Follow,
     foreign_key: :follower_id,
     primary_key: :id
 
   has_many :followed,
-    class_name: :Follow,
-    foreign_key: :followed_id,
-    primary_key: :id
+    through: :followed_relationships,
+    source: :followed
 
   has_one_attached :propic
 end
