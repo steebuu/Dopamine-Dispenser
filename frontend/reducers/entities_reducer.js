1d3b1ef13dc08e73ab43c5b151980ac17fe163cc
Steve <T.AllGameMail@protonmail.com>
2020-02-03 14:01:22 -0500

    adds follows slice of state under entities

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -1,10 +1,12 @@
 import { combineReducers } from 'redux';
 import usersReducer from './users_reducer';
 import imagesReducer from './images_reducer';
+import followsReducer from './follows_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
-  images: imagesReducer
+  images: imagesReducer,
+  follows: followsReducer
 });
 
 export default entitiesReducer;
