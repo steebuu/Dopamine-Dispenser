bfd12303d887f6dcd2ea2dd352e6f17272c929df
Steve <T.AllGameMail@protonmail.com>
2020-02-12 17:21:53 -0500

    adds like model

--- app/models/like.rb
+++ app/models/like.rb
@@ -10,4 +10,16 @@
 #
 
 class Like < ApplicationRecord
+    validates :user_id, presence: true
+    validates :image_id, presence: true
+
+    belongs_to :user,
+    class_name: :User,
+    foreign_key: :user_id,
+    primary_key: :id
+
+    belongs_to :image,
+    class_name: :Image,
+    foreign_key: :image_id,
+    primary_key: :id
 end
