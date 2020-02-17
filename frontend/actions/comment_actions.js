f1b3dae594c9e7170edd7ae19feb6aa8d9934035
Steve <T.AllGameMail@protonmail.com>
2020-02-17 16:45:49 -0500

    fixes typo in RECEIVE_COMMENTS

--- frontend/actions/comment_actions.js
+++ frontend/actions/comment_actions.js
@@ -2,7 +2,7 @@ import * as commentAPI from '../util/comment_api_util';
 
 export const RECEIVE_COMMENT = "RECEIVE_COMMENT";
 export const REMOVE_COMMENT = "REMOVE_COMMENT";
-export const RECEIVE_COMMENTS = "RECEIVE_COMMENT";
+export const RECEIVE_COMMENTS = "RECEIVE_COMMENTS";
 
 export const receiveComment = comment => {
     return {
