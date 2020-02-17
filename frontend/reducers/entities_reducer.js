08e8405a38b308c97f1b3d9bdf240c35768dd114
Steve <T.AllGameMail@protonmail.com>
2020-02-17 15:06:20 -0500

    adds comments reducer to entities reducer

--- frontend/reducers/entities_reducer.js
+++ frontend/reducers/entities_reducer.js
@@ -3,12 +3,14 @@ import usersReducer from './users_reducer';
 import imagesReducer from './images_reducer';
 import followsReducer from './follows_reducer';
 import likesReducer from './likes_reducer';
+import commentsReducer from './comments_reducer';
 
 const entitiesReducer = combineReducers({
   users: usersReducer,
   images: imagesReducer,
   follows: followsReducer,
-  likes: likesReducer
+  likes: likesReducer,
+  comments: commentsReducer
 });
 
 export default entitiesReducer;
