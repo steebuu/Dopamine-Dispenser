1fdbd4341fc21bb34d0e8b1b0b136c1b030d4a74
Steve <T.AllGameMail@protonmail.com>
2020-02-19 23:20:36 -0500

    comments now show user profile pictures/links to profile pages

--- frontend/components/comments/comment_index_item.jsx
+++ frontend/components/comments/comment_index_item.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import {Link} from 'react-router-dom';
 
 export default class CommentIndexItem extends Component {
     constructor(props){
@@ -10,9 +11,26 @@ export default class CommentIndexItem extends Component {
         if (!comment || !comment.user || !comment.body) {
             return null;
         }
+        const user = users[comment.user_id];
+        let propic;
+        if (!user.propicUrl) {
+            propic = window.propic
+        } else (
+            propic = user.propicUrl
+        )
         return (
             <div className="comment">
-               <div className="main-feed-caption-username">{comment.user}</div>
+                <div className="image-modal-info-user-div">
+                    <Link to={`/users/${user.id}`} className="name-link">
+                        <div className="comment-prof-pic-div">
+                            <img src={propic} className="comment-prof-pic" />
+                        </div>
+                        <div className="comment-username-div">
+                            {user.username}
+                        </div>
+                    </Link>
+                </div>
+               {/* <div className="main-feed-caption-username">{comment.user}</div> */}
                <div className="comment-body">{comment.body}</div>         
             </div>
         )
